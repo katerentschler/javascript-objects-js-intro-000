06c637f30538abf56e810bbfe9162b0e1a3693de
Kate Rentschler <nkpe@outlook.com>
2018-10-03 18:29:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,5 @@
-var playlist = new Object({artistName:songTitle});
+var playlist = new Object();
+var playlist = {artistName:songTitle};
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
